0d59017d860913e95fcdfe51b05cb99836e3259a
Minh <minh.nguyen1@uoit.net>
2016-09-07 09:41:09 -0400

    Initial Commit

---
+++ Assets/CopyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopyScript : Photon.MonoBehaviour {
+
+    public GameObject AvatarHead;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (photonView.isMine)
+            transform.position = AvatarHead.transform.position;
+	
+	}
+}
